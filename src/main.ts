ddc6717e9ee4af0326eb1d3f814a5043f5ad3b23
Nico Linnemann <nicochan95.anime@gmail.com>
2019-03-31 19:23:13 +0200

    Add Vuetify to app

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
 import Vue from 'vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
 import App from './App';
 import router from './router';
 import store from './store';
+
+Vue.use(Vuetify, {
+  iconfont: 'md',
+});
 
 Vue.config.productionTip = false;
 
